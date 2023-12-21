29017dc6ac05a1fbfedbde7ab528db4a452f2bc1
JordanWalling <jordanwalling1991@gmail.com>
2023-12-21 17:27:19 +1100

    comple first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (array) => {
+  return array.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (array) => {
+  return array.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (num) => {
+  return (fare) => {
+    return fare * num;
+  };
+};
+
+const fareMultiplierByTwo = createFareMultiplier(2);
+
+const fareMultiplierByFive = createFareMultiplier(5);
+
+const fare = 10;
+const fareMultipliedByTwo = fareMultiplierByTwo(fare);
+
+const fareMultipliedByFive = fareMultiplierByFive(fare);
+
+// console.log(fareMultipliedByTwo); // = 20 (doubled fare)
+
+// console.log(fareMultipliedByFive); // = 50 (fare * 5)
+
+// FARE DOUBLER
+const fareDoubler = createFareMultiplier(2);
+
+// FARE TRIPLER
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (array, returnFirstTwoDrivers) => {
+  return returnFirstTwoDrivers(array);
+};
